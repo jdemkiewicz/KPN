c0f750eb8ec2d549be3d915b456d5481a2c9d620
jdemkiewicz <jakub@demkiewicz.pl>
2017-08-29 18:32:32 +0200

    travis conf file

--- app/src/main/java/com/jdemkiewicz/kpn/MainActivity.java
+++ app/src/main/java/com/jdemkiewicz/kpn/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         addScore(gameResult);
         setResult();
         checkScore();
+        
     }
 
     private void showAlertDialog(String message) {
